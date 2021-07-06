3ba682697b31a04f7ca3598092e6df206d4f9a73
HaikalPWPB <haikalg2003@gmail.com>
2021-07-06 13:47:01 +0700

    Refactoring SearchScreen

--- lib/network/news_service.dart
+++ lib/network/news_service.dart
@@ -1,7 +1,8 @@
-import 'package:news/model/News.dart';
 import 'api.dart';
 import 'dart:convert';
+import 'package:news/model/News.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class NewsService {
   static String baseUrl = '/news';
@@ -17,6 +18,37 @@ class NewsService {
     for (var a in jsonData) {
       News post =
           News(a['title'], a['content'], a['created_at'], a['updated_at']);
+      news.add(post);
+    }
+    print(news.length);
+    return news;
+  }
+
+    Future<List<News>> searchNews(String keyword) async {
+    // Post text to laravel api
+    print(keyword);
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var token = jsonDecode(localStorage.getString('token'))['token'];
+    var response = await http.post(
+        Uri.parse('https://haikal.cyberwarrior.co.id/api/v1/news/search'),
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token'
+          },
+          body: jsonEncode(<String, String>{
+            'keyword': keyword
+          }),
+        );
+        
+    if(response.statusCode == 200) {
+      print('Hello world');
+    }
+    var jsonData = jsonDecode(response.body);
+    print(jsonData.length);
+    List<News> news = [];
+
+    for(var a in jsonData) {
+      News post = News(a['title'], a['content'], a['created_at'], a['updated_at']);
       news.add(post);
     }
     print(news.length);
--- lib/screen/search.dart
+++ lib/screen/search.dart
@@ -1,12 +1,9 @@
-import 'dart:convert';
-// import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:news/model/News.dart';
-import 'package:http/http.dart' as http;
 import 'package:news/screen/news.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:news/helper/date.dart';
+import 'package:news/network/news_service.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -16,37 +13,6 @@ class SearchScreen extends StatefulWidget {
 class SearchScreenState extends State<SearchScreen> {
   final SearchBarController<News> _searchBarController = SearchBarController();
   bool _isReplay = false;
-
-  Future<List<News>> _getNews(String keyword) async {
-    // Post text to laravel api
-    print(keyword);
-    SharedPreferences localStorage = await SharedPreferences.getInstance();
-    var token = jsonDecode(localStorage.getString('token'))['token'];
-    var response = await http.post(
-        Uri.parse('https://haikal.cyberwarrior.co.id/api/v1/news/search'),
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer $token'
-          },
-          body: jsonEncode(<String, String>{
-            'keyword': keyword
-          }),
-        );
-        
-    if(response.statusCode == 200) {
-      print('Hello world');
-    }
-    var jsonData = jsonDecode(response.body);
-    print(jsonData.length);
-    List<News> news = [];
-
-    for(var a in jsonData) {
-      News post = News(a['title'], a['content'], a['created_at'], a['updated_at']);
-      news.add(post);
-    }
-    print(news.length);
-    return news;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,7 +27,7 @@ class SearchScreenState extends State<SearchScreen> {
             placeHolder: Text('Search by title...'),
             cancellationWidget: Icon(Icons.cancel),
             searchBarController: _searchBarController,
-            onSearch: _getNews,
+            onSearch: NewsService().searchNews,
             onItemFound: (News news, int index) {
               return Container(
                 child: ListTile(
